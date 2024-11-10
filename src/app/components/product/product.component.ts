bf6b20e010e2cf3b9dfde535aac79cc09bca8fc4
pujayasurya <pujayasurya1997@gmail.com>
2024-11-10 21:54:46 +0530

    filter and sorting

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -13,11 +13,12 @@ export class ProductComponent implements OnInit {
   filterText: string = "";
   products: Product[] = [];
   filteredProducts: Product[] = [];
+  sortDirection: { [key: string]: 'desc' | 'asc' } = {};
   faSortDesc = faSortDesc;
   faSortAsc = faSortAsc;
   faEdit = faEdit;
   faTrash = faTrash;
-  
+
   constructor(
     private productService: ProductService,
     private router: Router
@@ -38,6 +39,26 @@ export class ProductComponent implements OnInit {
     this.filteredProducts = this.products.filter(product =>
       product.name.toLowerCase().includes(this.filterText.toLowerCase())
     );
+  }
+
+  toggleSort(sortField: string) {
+    if (!this.sortDirection[sortField]) {
+      this.sortDirection[sortField] = 'desc';
+    } else {
+      this.sortDirection[sortField] = this.sortDirection[sortField] === 'desc' ? 'asc' : 'desc';
+    }
+    this.sortProducts(sortField);
+  }
+
+  sortProducts(sortField: string) {
+    const direction = this.sortDirection[sortField];
+    this.products.sort((a: any, b: any) => {
+      if (direction === 'desc') {
+        return a[sortField] > b[sortField] ? 1 : a[sortField] < b[sortField] ? -1 : 0;
+      } else {
+        return a[sortField] < b[sortField] ? 1 : a[sortField] > b[sortField] ? -1 : 0;
+      }
+    });
   }
 
   deleteProduct(id: string): void {
--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -1,5 +1,6 @@
 export interface Product {
     id: string;
+    date: Date;
     name: string;
     category: string;
     price: number;
